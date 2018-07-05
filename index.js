2065a2513425f8281847de61b3991a30a0b9ed1e
acailly <ylliac14@gmail.com>
2018-07-05 22:48:48 +0200

    :sparkles: Improve description format

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const fs = require('fs')
-const Twitter = require('twitter')
-const RSS = require('rss');
+const fs = require("fs");
+const Twitter = require("twitter");
+const RSS = require("rss");
+const tweetToHTML = require("tweet-to-html");
 
-module.exports = function (vorpal) {
+const photoConfig = {
+  photoSize: "large" // Any size supported by the `media` entity (thumb, small, medium...)
+};
+
+module.exports = function(vorpal) {
   vorpal
-    .command('twitter-to-rss')
-    .description('Create a RSS feed from your Twitter home timeline')
-    .action(function (args, callback) {
+    .command("twitter-to-rss")
+    .description("Create a RSS feed from your Twitter home timeline")
+    .action(function(args, callback) {
       const {
         consumer_key,
         consumer_secret,
@@ -15,7 +20,7 @@ module.exports = function (vorpal) {
         feedCount,
         feedTitle,
         feedFilePath
-      } = vorpal.config.twitter_rss
+      } = vorpal.config.twitter_rss;
 
       //From https://apps.twitter.com/
       const client = new Twitter({
@@ -28,33 +33,41 @@ module.exports = function (vorpal) {
       const params = {
         count: feedCount,
         //https://developer.twitter.com/en/docs/tweets/tweet-updates
-        tweet_mode: 'extended'
-      }
+        tweet_mode: "extended"
+      };
       // https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-home_timeline.html
-      client.get('statuses/home_timeline', params, function (error, tweets) {
+      client.get("statuses/home_timeline", params, function(error, tweets) {
         if (!error) {
           const feedOptions = {
             title: feedTitle
-          }
-          const feed = new RSS(feedOptions)
-          tweets.forEach(tweet => feed.item(tweetToRssItem(tweet)))
+          };
+          const feed = new RSS(feedOptions);
+          tweets.forEach(tweet => {
+            const rssItem = tweetToRssItem(tweet);
+            feed.item(rssItem);
+          });
+
           const xml = feed.xml({
             indent: true
-          })
-          fs.writeFileSync(feedFilePath, xml)
+          });
+          fs.writeFileSync(feedFilePath, xml);
+        } else {
+          console.error(error);
         }
-        callback()
+        callback();
       });
-    })
-}
+    });
+};
 
 function tweetToRssItem(tweet) {
+  const description = tweetToHTML.parse(tweet, photoConfig).html;
+
   return {
     title: tweet.full_text,
-    description: tweet.full_text,
-    url: `https://twitter.com/statuses/${tweet.id_str}`,
+    description: description,
+    url: `https://twitter.com/i/web/status/${tweet.id_str}`,
     guid: tweet.id_str,
     author: tweet.user.name,
     date: tweet.created_at
-  }
+  };
 }
